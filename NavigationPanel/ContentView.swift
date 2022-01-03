07a35c2481f5ecf0f8de4b911c088348424e14b4
Kriti Agarwal <kriti.agarwal@nihilent.com>
2022-01-03 11:31:51 +0530

    Initial Commit

---
+++ NavigationPanel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationPanel
+//
+//  Created by Kriti Agarwal on 03/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationPanel/NavigationPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPanelApp.swift
+//  NavigationPanel
+//
+//  Created by Kriti Agarwal on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
